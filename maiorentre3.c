56a66f53c0380477cb0feabd95d77f27546d7af6
BianchiLuis <bianchi.luis@unifesp.br>
2026-04-09 21:47:30 -0300

    Create maiorentre3.c

---
+++ maiorentre3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c;
+    int MaiorAB, Maior;
+    scanf("%d %d %d", &a, &b, &c);
+    MaiorAB = (a + b + abs(a - b)) / 2;
+    Maior = (MaiorAB + c + abs(MaiorAB - c)) / 2;
+    printf("%d eh o maior\n", Maior);
+
+    return 0;
+}
